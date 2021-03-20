5912e6509f7277a991b10ad157e78416f076c030
Ilya <iluha3211@gmail.com>
2021-03-20 06:21:30 +0200

    switch theme

--- js/script.js
+++ js/script.js
@@ -5,7 +5,8 @@ document.addEventListener('DOMContentLoaded', () => {
 	const container = document.querySelector(".calculator__field"),
 		btnHiddenMenuOpen = document.querySelector('.cacluculator__hidden-menu'),
 		btnHiddenMenuClose = document.querySelector('.hidden-menu__close'),
-		hiddenMenu = document.querySelector('.hidden-menu')
+		hiddenMenu = document.querySelector('.hidden-menu'),
+		switchThemeBtn = document.querySelector('.calculator__top-theme')
 		;
 
 	btnHiddenMenuOpen.addEventListener('click', (e) => {
@@ -16,6 +17,7 @@ document.addEventListener('DOMContentLoaded', () => {
 		e.preventDefault();
 		hiddenMenu.classList.remove('hidden-menu--active');
 	});
+	switchThemeBtn.addEventListener('click', () => switchTheme(document.body, 'dark-theme'));
 	scrollToBottom(container);
 
 });
@@ -24,4 +26,13 @@ function scrollToBottom(container) {
 	container.scrollTop = container.scrollHeight;
 }
 
+function switchTheme(item, classActive) {
+	item.classList.toggle(classActive);
+	const img = item.querySelector('img');
+	if (item.classList.contains(classActive)) {
+		img.src = "./icons/moon.png";
+	} else {
+		img.src = "./icons/sun.png";
+	}
+}
 
